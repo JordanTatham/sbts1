26d79c7cbc149dca938be319bf68031bf788921c
JordanTatham <jordan@servicebythesea.co.uk>
2025-07-09 22:29:10 +0100

    Delete service-by-the-sea directory

--- service-by-the-sea/app/edit-guide/page.tsx
+++
@@ -1,5 +0,0 @@
-import { WebsiteEditorGuide } from "@/components/website-editor-guide"
-
-export default function EditGuidePage() {
-  return <WebsiteEditorGuide />
-}
--- service-by-the-sea/app/netlify-guide/page.tsx
+++
@@ -1,5 +0,0 @@
-import { NetlifyDeploymentGuide } from "@/components/netlify-deployment-guide"
-
-export default function NetlifyGuidePage() {
-  return <NetlifyDeploymentGuide />
-}
